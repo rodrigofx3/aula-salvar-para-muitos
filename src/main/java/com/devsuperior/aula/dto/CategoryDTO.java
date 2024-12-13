f800dfc07665abe2b9c52ee28bc07b916c92860e
Rodrigo Faria Ramos <rodframos@outlook.com>
2024-12-13 11:31:29 -0300

    implement Product creation with ProductController and ProductService

--- src/main/java/com/devsuperior/aula/dto/CategoryDTO.java
+++ src/main/java/com/devsuperior/aula/dto/CategoryDTO.java
@@ -1,4 +1,27 @@
 package com.devsuperior.aula.dto;
 
+import com.devsuperior.aula.entities.Category;
+
 public class CategoryDTO {
+
+    Long id;
+    String name;
+
+    public CategoryDTO(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public CategoryDTO(Category entity) {
+        id = entity.getId();
+        name = entity.getName();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
--- src/main/java/com/devsuperior/aula/services/ProductService.java
+++ src/main/java/com/devsuperior/aula/services/ProductService.java
@@ -1,4 +1,32 @@
 package com.devsuperior.aula.services;
 
+import com.devsuperior.aula.dto.CategoryDTO;
+import com.devsuperior.aula.dto.ProductDTO;
+import com.devsuperior.aula.entities.Category;
+import com.devsuperior.aula.entities.Product;
+import com.devsuperior.aula.repositories.CategoryRepository;
+import com.devsuperior.aula.repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ProductService {
+
+    @Autowired
+    private ProductRepository productRepository;
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    public ProductDTO create(ProductDTO dto) {
+        Product entity = new Product();
+        entity.setName(dto.getName());
+        entity.setPrice(dto.getPrice());
+        for (CategoryDTO catDto : dto.getCategories()) {
+            Category category = categoryRepository.getReferenceById(catDto.getId());
+            entity.getCategories().add(category);
+        }
+        entity = productRepository.save(entity);
+        return new ProductDTO(entity);
+    }
+
 }
